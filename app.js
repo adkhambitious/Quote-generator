ef91bcf2e6058a234591813d8cfbe72a819c0485
Adkham Badalbaev <monterey-bay@mail.ru>
2020-09-15 14:09:48 +0300

    add # to the href of a tag

--- app.js
+++ app.js
@@ -1,34 +1,39 @@
-// Naming of variables
-let getRandomQuote = document.querySelector('.random');
-
 // Объект в котором будут данные по цитате
 const phrase = {};
+
 // Selected elements
 const oneQuoteReflection = document.querySelector('.p');
 const authorReflection = document.querySelector('.initials');
+const authorsArea = document.querySelector('.area');
 // Clicking on random button
 
-
+// Naming of variables
+let getRandomQuote = document.querySelector('.random');
 // Getting data from API-provider
-getRandomQuote.onclick = function(event) {
+getRandomQuote.onclick = function() {
     const api = 'https://quote-garden.herokuapp.com/api/v2/quotes/random';
     fetch(api)
         .then((response) => {
             let data = response.json();
+            console.log('Привет!') // 1-st
+            return data;
         })
         .then((data) => {
+            console.log(data); // 2-nd
             phrase.id = data.id;
-            phrase.quoteText = data.quoteText;
-            phrase.quoteAuthor = data.quoteAuthor;
+            phrase.quoteText = data.quote.quoteText;
+            phrase.quoteAuthor = data.quote.quoteAuthor;
+            phrase.field = data.quote.quoteGenre;
         })
         .then(function() {
             displayQuote();
         });
 };
-alert (phrase);
+console.log(phrase); // 3 -rd
 function displayQuote() {
     oneQuoteReflection.innerHTML = `${phrase.quoteText}`;
     authorReflection.innerHTML = `${phrase.quoteAuthor}`;
+    authorsArea.innerHTML = `${phrase.field}`
 };
 
 
